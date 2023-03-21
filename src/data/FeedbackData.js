7c33d9a3824736580bea4327e66ba7f04e07ab64
diveshwatwani <60432327+diveshwatwani@users.noreply.github.com>
2023-03-22 01:44:28 +0530

    v2.0

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData=[
+    {
+        id:1,
+        rating:10,
+        text:'hello text bot bot text sit hui volupatishm ipsa kipsa falana dimkana',
+    },
+    {
+        id:2,
+        rating:8,
+        text:'hello te text sit hui volupatishm ipsa kipsa falana dimkana',
+    },
+    {
+        id:3,
+        rating:6,
+        text:'hello text bot patishm ipsa kipsa falana dimkana',
+    },
+    {
+        id:4,
+        rating:4,
+        text:'hello tt bothm ipsa kipsa falana dimkana',
+    },
+    {
+        id:5,
+        rating:9,
+        text:'hello tpatishm ipsa kimkana buybiwfy fgyfwgg ihioh78ttguoy',
+    }
+]
+
+export default FeedbackData
